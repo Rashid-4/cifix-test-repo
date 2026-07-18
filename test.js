f0f8d7b3d1442ec435949e499e4c4e71499ac089
Rashid Ali Khan <rashidalikhan99@gmail.com>
2026-07-18 23:31:29 +0530

    add lodash usage without installing it

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const lodash = require('lodash');
 const app = express();
 
-app.get('/', (req, res) => res.send('hello'));
+app.get('/', (req, res) => res.send(lodash.capitalize('hello')));
 
 console.log('App initialized');
 console.log('Tests passed!');
