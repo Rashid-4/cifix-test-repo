f93a8a9da8c7a804cab3093e25f72f4e66cd05a5
Rashid Ali Khan <rashidalikhan99@gmail.com>
2026-07-19 00:02:45 +0530

    trigger heap OOM in CI

--- test.js
+++ test.js
@@ -2,6 +2,13 @@ const express = require('express');
 const lodash = require('lodash');
 const app = express();
 
+// Simulate heap OOM in CI
+const args = process.argv.slice(2);
+if (args.includes('--ci')) {
+  const arr = [];
+  while (true) { arr.push(new Array(1000000).fill('x')); }
+}
+
 app.get('/', (req, res) => res.send(lodash.capitalize('hello')));
 
 console.log('App initialized');
